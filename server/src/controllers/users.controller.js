bc1ee625e917f8b6d0652019b672984782578b2c
felixGlez <felixgonzalezgarcia2010@gmail.com>
2024-05-28 20:11:15 +0200

    Back finished

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -1,4 +1,5 @@
 const { readFile } = require('fs');
+const { v4 } = require('uuid');
 
 const usersController = {};
 
@@ -11,6 +12,7 @@ usersController.getAllUsers = async (req, res) => {
   try {
     const users = await fsPromises.readFile(fileData);
     const parsedUsers = JSON.parse(users);
+
     res.send(parsedUsers);
   } catch (err) {
     return res.send('Error get');
@@ -19,15 +21,53 @@ usersController.getAllUsers = async (req, res) => {
 
 usersController.createUser = async (req, res) => {
   const newUser = req.body;
-  newUser.userId = '0001';
+  newUser.userId = v4();
   try {
-    const users = fsPromises.readFile(fileData);
+    const users = await fsPromises.readFile(fileData);
     const parsedUsers = JSON.parse(users);
 
     const updatedUsersList = [...parsedUsers, newUser];
-    console.log(updatedUsersList);
+
+    await fsPromises.writeFile(fileData, JSON.stringify(updatedUsersList));
+
+    res.send(updatedUsersList);
   } catch (error) {
     return res.send('Error post');
+  }
+};
+
+usersController.updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  try {
+    const users = await fsPromises.readFile(fileData);
+    const parsedUsers = JSON.parse(users);
+
+    const userIndex = parsedUsers.findIndex(user => user.userId === id);
+    parsedUsers[userIndex].name = name;
+    parsedUsers[userIndex].email = email;
+
+    await fsPromises.writeFile(fileData, JSON.stringify(parsedUsers));
+
+    res.send(parsedUsers);
+  } catch (error) {
+    return res.send('Error patch');
+  }
+};
+
+usersController.deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const users = await fsPromises.readFile(fileData);
+    const parsedUsers = JSON.parse(users);
+
+    const filteredUsers = parsedUsers.filter(user => user.userId !== id);
+
+    await fsPromises.writeFile(fileData, JSON.stringify(filteredUsers));
+
+    res.send(filteredUsers);
+  } catch (error) {
+    return res.send('Error delete');
   }
 };
 
--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -1,9 +1,11 @@
-const express = require('express');
 const usersController = require('../controllers/users.controller');
 
+const express = require('express');
 const usersRoutes = express.Router();
 
 usersRoutes.get('/', usersController.getAllUsers);
 usersRoutes.post('/', usersController.createUser);
+usersRoutes.patch('/:id', usersController.updateUser);
+usersRoutes.delete('/:id', usersController.deleteUser);
 
 module.exports = usersRoutes;
